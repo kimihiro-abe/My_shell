efd0c6ec6276b8a594a4066be0c311dc8105b2b5
kimihiro-abe <kimihiro.abe@gmail.com>
2024-01-20 16:32:16 +0900

    first commit

---
+++ resumeWorkFolder_batch/resumeWorkFolder.bat
@@ -0,0 +1,8 @@
+rem 月～金の間はPCつけっぱで、金曜日にPC電源を落とす時などに、
+rem resumeWork.txtに作業途中のフォルダや開いていたエクセルのパスを記載しておき、
+rem 作業再開時にこのバッチファイルをダブルクリックして楽するメソッド
+
+@echo off
+for /f %%a in (resumeWorkFolderList.txt) do (
+  explorer.exe "%%a"
+)
