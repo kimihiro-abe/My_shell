d0d2abbea12d9d4edd6063217e98a061919fa4d8
kimihiro-abe <kimihiro.abe@gmail.com>
2025-03-02 02:08:32 +0900

    arr_updateYear.sh update

--- arr_updateYear.sh
+++ arr_updateYear.sh
@@ -2,5 +2,8 @@
 #execution	: ./updateYear.sh
 #auther		: Kimihiro Abe
 #memo		: for Audacity meta data
-#ディレクトリ内にある、全xmlファイル内のyearの箇所を置換する
-sed -s -i s/2023/2024/g *.xml
+#           : ディレクトリ内にある、全xmlファイル内のyearの箇所を置換する
+#created    : 2024/01/09
+#updated    : 2025/03/02
+
+sed -s -i s/2024/2025/g *.xml
